cdab5ab69b2c1f96501ba6af4ab7ed7cc3766c7e
Salman8160 <salmantabrezmd@gmail.com>
2026-02-01 07:32:12 -0600

    Fix ESLint v9 flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+  recommendedConfig: js.configs.recommended,
+});
+
+export default [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "dist/**",
+      ".vercel/**",
+    ],
+  },
+];
